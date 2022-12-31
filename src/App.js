c7dbb6b7cf19ca127484f9a8c8ea0c575fddfb9b
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-12-31 13:06:09 -0500

    initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {Chart} from './components/chart'
+
+function App() {
+  return (
+    <div className="App">
+      <Chart/>
+    </div>
+  );
+}
+
+export default App;
